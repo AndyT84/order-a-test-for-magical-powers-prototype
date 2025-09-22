ccbfe69d98bd96f244482d36a7139c263e5c314d
AndyT84 <108937222+AndyT84@users.noreply.github.com>
2025-09-22 13:58:33 +0000

    Added ineligible logic

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,29 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// Called when answering question about whether NHS number is known
+router.post('/example-answer', (req, res) => {
 
+  // Make a variable and give it the value from 'nhsNumberKnown'
+  const magicPowersExample = req.session.data.magicPowersExample
+
+  // Check whether the variable matches a condition
+  if (magicPowersExample === 'Yes') {
+
+    // Send user to a page where they’ll enter their NHS number
+    res.redirect('/details')
+
+  } else if (magicPowersExample === 'No') {
+
+    // Send user to a page where they can find their NHS number
+    res.redirect('/ineligible')
+
+  } else {
+
+    // Send user back to the question page
+    res.redirect('/example')
+
+  }
+})
 module.exports = router
+
